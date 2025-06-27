78b365dd8659e2a1061649ac998987b9b88233db
shouvik214 <shouvik.mondal2005@gmail.com>
2025-06-27 12:09:30 +0530

    vercel file added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL
+
 export const generateQuestionsAPI = async (formData) => {
   try {
-    const response = await axios.post('http://localhost:3000/api/v1/questions', formData)
+    const response = await axios.post(`${BASE_URL}/api/v1/questions`, formData)
     return response.data // contains { questions: [...] }
   } catch (error) {
     // Axios-specific error handling
