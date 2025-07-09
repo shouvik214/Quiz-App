ad98c02d571740f20353a9a18b68ec9beb0a4115
shouvik214 <shouvik.mondal2005@gmail.com>
2025-07-09 20:49:37 +0530

    Making changes for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_BACKEND_PROD_URL;
 
 export const generateQuestionsAPI = async (formData) => {
   try {
